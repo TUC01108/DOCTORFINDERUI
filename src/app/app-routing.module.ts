1d74055479a34a54b3abfb66995a4d3a4aa64fa2
TUC01108 <edwinthomaswinter@gmail.com>
2022-03-26 11:27:29 -0700

    routing and app.component.html update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentAddComponent } from './components/appointment-add/appointment-add.component';
+import { AppointmentInfoComponent } from './components/appointment-info/appointment-info.component';
+import { BillAddComponent } from './components/bill-add/bill-add.component';
+import { BillInfoComponent } from './components/bill-info/bill-info.component';
+import { DoctorAddComponent } from './components/doctor-add/doctor-add.component';
+import { DoctorInfoComponent } from './components/doctor-info/doctor-info.component';
 import { LoginInfoComponent } from './components/login-info/login-info.component';
 import { LoginComponent } from './components/login/login.component';
 import { PatientAddComponent } from './components/patient-add/patient-add.component';
@@ -13,6 +19,12 @@ const routes: Routes = [
   { path:'loginInfo', component : LoginInfoComponent},
   { path: 'patientAdd', component : PatientAddComponent},
   { path: 'patientInfo', component : PatientInfoComponent},
+  { path: 'doctorAdd', component : DoctorAddComponent},
+  { path: 'doctorInfo', component : DoctorInfoComponent},
+  { path: 'billAdd', component : BillAddComponent},
+  { path: 'billInfo', component : BillInfoComponent},
+  { path: 'appointmentAdd', component : AppointmentAddComponent},
+  { path: 'appointmentInfo', component : AppointmentInfoComponent},
 ];
 
 @NgModule({
